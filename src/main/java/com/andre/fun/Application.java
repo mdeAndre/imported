f3d0ae3ea3d32df3527428bce3135de3cd2b7d21
andre <angry_front@example.com>
2016-10-31 12:21:13 +0200

    initial commit

---
+++ src/main/java/com/andre/fun/Application.java
@@ -0,0 +1,11 @@
+package com.andre.fun;
+
+/**
+ * Created by Khemraev Andre on 10/31/16.
+ */
+public class Application {
+
+    public static void main(String[] args) {
+        System.out.println("blah");
+    }
+}
