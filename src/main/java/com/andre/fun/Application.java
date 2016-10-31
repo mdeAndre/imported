aef4d541ca1c6d790a24697fdb6b42ddb158e442
andre <angry_front@example.com>
2016-10-31 12:57:12 +0200

    adds comment

--- src/main/java/com/andre/fun/Application.java
+++ src/main/java/com/andre/fun/Application.java
@@ -1,11 +1,16 @@
 package com.andre.fun;
 
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by Khemraev Andre on 10/31/16.
  */
 public class Application {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world! This is Earth Prime!");
+        //sample
+        String string = Stream.of("I", "do", "want", "to", "be", "a", "git", "guru!").collect(Collectors.joining(" "));
+        System.out.println(string);
     }
 }
